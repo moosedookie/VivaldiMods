c33ab758d31c227664bd8fe094b5d906d8c09620
moosedookie <moosedookie@moosedookie.me>
2019-04-06 19:29:56 -0500

    ...

---
+++ user_mods/.old/extension_toggle.js
@@ -0,0 +1,73 @@
+function extensionToggle() {
+
+// Add extension IDs of buttons you want to keep permanently visible to the array. Remove example IDs.
+
+    var selectIDs =
+        [
+        'oeopbcgkkoapgobdbedcemjljbihmemj',
+        'cjpalhdlnbpafiamejdnhcphjbkeiagm',
+        'ffpgijchhhkhnokafdeklpllijgnbche',
+        'eignhdfgaldabilaaegmdfbajngjmoke',
+        'edacconmaakjimmfgnblocblbcdcpbko',
+        ];
+
+// Add extension IDs of buttons you want to hide to the array. Remove example IDs.
+
+    var hideIDs =
+        [
+        'jgpmhnmjbhgkhpbgelalfpplebgfjmbf'
+        ];
+
+    // create the button
+    const adr = document.querySelector('.toolbar-addressbar.toolbar');
+    var div = document.createElement('div');
+    div.classList.add('button-group');
+    div.innerHTML = '<button id="togglemod" class="button-toolbar toggle-extensions-group" title="Toggle mod" tabindex="0" style="margin-left:0px"><svg width="4" height="16" viewBox="0 0 4 16" xmlns="http://www.w3.org/2000/svg"><path d="M2 4c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zm0 6c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zm0 6c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2z"></path></svg></button>';
+    adr.appendChild(div);
+
+    // startup setting
+    const startup = document.querySelectorAll('button.button-toolbar.browserAction-button');
+    const togstat = document.getElementById('togglemod');
+    for (var i=0; i < startup.length; i++) {
+        if (selectIDs.indexOf(startup[i].id) != -1) {
+            startup[i].style.display = 'flex';
+        }
+        else {
+            startup[i].style.display = 'none';
+        }
+    }
+
+    // toggle logic
+    togstat.addEventListener('click', function() {
+        const buttons = document.querySelectorAll('button.button-toolbar.browserAction-button');
+        if (togstat.classList.contains('expanded')) {
+            togstat.classList.remove('expanded');
+        }
+        else {
+            togstat.classList.add('expanded');
+        }
+        for (var i=0; i < buttons.length; i++) {
+            if (hideIDs.indexOf(buttons[i].id) != -1) {
+                buttons[i].style.display = 'none';
+            }
+            else if (selectIDs.indexOf(buttons[i].id) != -1 || togstat.classList.contains('expanded')) {
+                buttons[i].style.display = 'flex';
+            }
+            else {
+                buttons[i].style.display = 'none';
+            }
+        }
+    });
+};
+
+// Loop waiting for the browser to load the UI. You can call all functions from just one instance.
+
+setTimeout(function wait() {
+    const browser = document.getElementById('browser');
+    if (browser) {
+        extensionToggle();
+    }
+    else {
+        setTimeout(wait, 300);
+    }
+}, 300);
